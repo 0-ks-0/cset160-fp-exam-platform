8830dc08a58c02a29d5a94afc130f9bca2f509de
0-ks-0 <150286794+0-ks-0@users.noreply.github.com>
2024-05-14 18:52:39 -0400

    Add assignment name when taking one
    Redirects to /assignments if assignment id does not exist

--- src/server.py
+++ src/server.py
@@ -464,10 +464,16 @@ def route_create_assignment():
 
 @app.route("/take_test/<id>")
 def take_test(id):
+	assignment_name = get_query_rows(f"select * from `assignments` where `id` = {id};")
+
+	if len(assignment_name) < 1:
+		return redirect("/assignments")
+
 	questions = get_query_rows(f"select * from `assignment_questions` where `assignment_id` = {id};")
 
 	return render_template(
 		"assignment_take.html",
+		assignment_name = assignment_name[0].title,
 		questions = questions
 	)
 # End of routes
